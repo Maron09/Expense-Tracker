39badfbeb908910974006841cd14a6d02ac469c6
chimaroke onyebi <maroks216@gmail.com>
2023-02-20 17:10:39 +0100

    first commit

---
+++ analysis/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'expenses/index.html')
+
+
+
+def add_expenses(request):
+    return render(request, 'expenses/add_expenses.html')
